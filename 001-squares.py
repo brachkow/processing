41aa6aed7d6b7031521b11c432c4ca2a2f8f4ccf
Timur Brachkov <33203187+brachkoff@users.noreply.github.com>
2020-06-26 12:11:14 +0300

    improvements and cleanup for 003
    cleanup for 001
    
    indentation fixes

--- 001-squares.py
+++ 001-squares.py
@@ -1,24 +1,21 @@
 def setup():
-    size(512,512)
-    rectMode(CENTER)
-    noStroke()
+  size(512,512)
+  rectMode(CENTER)
+  noStroke()
 
 def draw():
-    clear()
-    background(255)
-    side = width / 2
+  clear()
+  background(255)
+  side = width / 2
 
-    for i in range(0, 13):
-        pushMatrix()
-        translate(width / 2, height / 2)
-        if (i % 2 != 0):
-            fill(255);
-        else:
-            fill(0);
-        rotate(radians(frameCount) * 2 * pow(-1, i))
-        rect(0, 0, side, side)
-        popMatrix()
-        side = side / sqrt(2)
-
-    if (frameCount <= 360):
-        saveFrame()
+  for i in range(0, 13):
+    pushMatrix()
+    translate(width / 2, height / 2)
+    if (i % 2 != 0):
+      fill(255);
+    else:
+      fill(0);
+    rotate(radians(frameCount) * 2 * pow(-1, i))
+    rect(0, 0, side, side)
+    popMatrix()
+    side = side / sqrt(2)
--- 003-circle.py
+++ 003-circle.py
@@ -1,14 +1,12 @@
 def setup():
-    size(2048,2048)
-    ellipseMode(CENTER)
-    background(255)
+  size(512,512)
+  ellipseMode(CENTER)
+  background(255)
 
 def draw():
   r = width / 2
   translate(r, r)
-  x = r / 2 * sin(radians(frameCount % 360)) * cos(frameCount)
-  y = r / 2 * cos(radians(frameCount % 360))
+  x = r / 2 * sin(radians(frameCount)) * cos(frameCount)
+  y = r / 2 * cos(radians(frameCount))
   d = width * 0.1
   ellipse(x, y, d, d)
-  if (frameCount <= 180):
-    saveFrame()
